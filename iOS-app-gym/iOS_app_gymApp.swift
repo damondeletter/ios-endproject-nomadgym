16ad6ed91ccc80c42f80456a6d67a7fa9b56309f
damondeletter <damon.deletter@student.hogent.be>
2022-12-18 11:44:08 +0100

    Initial Commit

---
+++ iOS-app-gym/iOS_app_gymApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_app_gymApp.swift
+//  iOS-app-gym
+//
+//  Created by Damon De Letter on 18/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS_app_gymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
